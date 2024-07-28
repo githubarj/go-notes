e8d288b8c707ae991607e50553824469cf7aaf4b
Richard Githuba <githuba9520@gmail.com>
2024-07-28 17:59:30 +0300

    maps

---
+++ maps/maps1/maps.go
@@ -0,0 +1,24 @@
+package main
+
+import "errors"
+
+func getUserMap(names []string, phoneNumbers []int) (map[string]user, error) {
+	userMap := make(map[string]user)
+	if len(names) != len(phoneNumbers) {
+		return nil , errors.New("Invalid Sizes")
+	}
+	for i := 0; i < len(names) ; i++ {
+		name := names[i]
+		phoneNumber := phoneNumbers[i]
+		userMap[name] = user {
+			name : name,
+			phoneNumber:  phoneNumber,
+		}
+	}
+	return userMap , nil
+}
+
+type user struct {
+	name        string
+	phoneNumber int
+}
