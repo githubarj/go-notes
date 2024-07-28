d9d75cae15f2d0c2e635b7b1d9365e4024514299
Richard Githuba <githuba9520@gmail.com>
2024-07-28 19:35:55 +0300

    distinct words

---
+++ maps/distinctWords/distictWords.go
@@ -0,0 +1,16 @@
+package main
+
+import "strings"
+
+func countDistinctWords(messages []string) int {
+	counter := make(map[string]int)	
+	for _, message := range messages {
+		words := strings.Fields(message)
+		for _, word := range words {
+			lowerWord := strings.ToLower(word)
+			counter[lowerWord]++
+		}
+
+	}
+	return len(counter)
+}
